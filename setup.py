4d2d59a851c501bb0ab576d5edb186fe8806d0d9
Romain Thoreau <romain.thoreau@magellium.fr>
2023-03-23 15:09:53 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TlseHypDataSet',
+    version='0.0.1',
+    description='A Python package to use and analyse the Toulouse Hyperspectral Data Set',
+    url='',
+    author='Romain Thoreau',
+    author_email='romain.thoreau@onera.fr',
+    packages=[],
+    install_requires=[]
+)
